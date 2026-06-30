5e4594f621567233001b7f6e1e3dc28078576fda
DavidAlonsoRomeroMedina <david.romero@tecdesoftware.edu.mx>
2026-06-30 05:17:41 -0600

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokeOracle.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
